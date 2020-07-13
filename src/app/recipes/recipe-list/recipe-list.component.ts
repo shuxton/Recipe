889d240eac8d34d6a3e071dbd4bdd72540fed507
shuxton <shubhamsnj@gmail.com>
2020-07-13 14:16:36 +0530

    initial front end

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes: Recipe[]=[
+  new Recipe('Thai','test','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWxDT6udBF3sCuxN3EqPh-cppdPFjFWmMhMA&usqp=CAU')
+];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
