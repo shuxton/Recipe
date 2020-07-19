588a56e918265bc15a0830fb92a4991a1f10522f
shuxton <shubhamsnj@gmail.com>
2020-07-19 19:26:38 +0530

    added observables(subject)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,10 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredients.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService{
-recipeSelected=new EventEmitter<Recipe>();
 
   private  recipes: Recipe[]=[
         new Recipe('Thai','Delicious extravaganza','https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWxDT6udBF3sCuxN3EqPh-cppdPFjFWmMhMA&usqp=CAU'
